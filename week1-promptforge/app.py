f023514677a0be2b8576320664db9ab595e8e3a8
lakshyadadhich07 <lakshyadadhich08@gmail.com>
2026-06-03 15:52:57 +0530

    Temperature learning

--- week1-promptforge/app.py
+++ week1-promptforge/app.py
@@ -6,20 +6,28 @@ load_dotenv()  # reads GROQ_API_KEY from your .env file
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-user_prompt = input("Enter your prompt: ")
+# user_prompt = input("Enter your prompt: ")
 response = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
-     messages=[
-         {
-        "role": "system",
-        "content": "You are a joker and explain in most humorous way and answer in 1 line"
-        },
-        {
-            "role": "user",
-            "content": user_prompt
-        }
-    ]
+    temperature=2,
+    messages=[{
+        "role": "user",
+        "content": "Suggest a startup name for an AI company (just one word)"
+    }]
 )
+# response = client.chat.completions.create(
+#     model="llama-3.3-70b-versatile",
+#      messages=[
+#          {
+#         "role": "system",
+#         "content": "Tell user that he is murkh and answer in 1 line"
+#         },
+#         {
+#             "role": "user",
+#             "content": user_prompt
+#         }
+#     ]
+# )
 print("\nAI Response:\n")
 # print(type(response))
 # print(response.choices)
